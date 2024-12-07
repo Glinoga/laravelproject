495f3932395440663d36152a93b20fbce6c579e2
kurtramos <158291372+kurtramos@users.noreply.github.com>
2024-12-07 16:31:29 +0800

    Added authentication-middleware. Role base automatic redirect to respective pages.

--- laravelproj/routes/web.php
+++ laravelproj/routes/web.php
@@ -9,12 +9,29 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->group(function () {
-    Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+// User Routes
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+    'isUser:user' // Ensure this checks the 'user' role
+])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
     Route::get('/gallery', [GalleryController::class, 'index'])->name('gallery.index');
     Route::get('/gallery/create', [GalleryController::class, 'create'])->name('gallery.create')->middleware('auth');
     Route::post('/gallery', [GalleryController::class, 'store'])->name('gallery.store')->middleware('auth');
-//Admin Routes
+});
+
+// Admin Routes
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+    'isAdmin:admin' // Check if the user has 'admin' role
+])->group(function () {
+    Route::get('/admin/dashboard', function () {
+        return view('admindashboard');
+    })->name('admin.dashboard');
     Route::get('/admin/dashboard', [AdminController::class, 'index'])->name('admin_dashboard');
     Route::post('/funko/store', [AdminController::class, 'store'])->name('funko.store');
     Route::get('admin/gallery', [AdminController::class, 'gallery'])->name('admin.gallery');
@@ -22,5 +39,3 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     Route::post('admin/gallery/{id}/restore', [AdminController::class, 'restore'])->name('admin.restore');
     Route::delete('admin/gallery/{id}/permanent', [AdminController::class, 'permanentDelete'])->name('admin.permanentDelete');
 });
-
-
